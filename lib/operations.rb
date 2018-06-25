51caf6cabdca9622da59339bda70dec2045291b6
Ana Bebic <anabebic.x@gmail.com>
2018-06-25 18:50:23 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,17 +1,19 @@
-def unsafe?(speed)
-   if speed > 40 && speed < 60
-     return true
-  else
-    return false
 
+
+
+
+
+def unsafe?(speed)
+	if speed > 60 || speed < 40
+		return true;
+	else
+		return false;
+	end
 end
 
 
 
 def not_safe?(speed)
-speed > 40 && speed < 60? "safe":"not_safe"
+	speed > 60 || speed < 40 ? true : false;
 end
-end
-	
-
 
